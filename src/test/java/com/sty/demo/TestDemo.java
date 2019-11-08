81e5bad8f41bf3737d8bf4d4f732e83653543024
shentianyu971121 <948653027@qq.com>
2019-11-08 14:46:20 +0800

    第一次上传

---
+++ src/test/java/com/sty/demo/TestDemo.java
@@ -0,0 +1,14 @@
+package com.sty.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDemo {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
